44e9fbd46a8da6731f678bdcc1c3691ba650f418
claraxuxu <claraxu1201@gmail.com>
2023-02-03 14:46:06 +0800

    fix Link from other lib; del useless font

--- web/src/component/NavBar.tsx
+++ web/src/component/NavBar.tsx
@@ -1,6 +1,5 @@
-import { Link } from "react-scroll";
 import "../main.scss"
-
+import { Link } from "react-router-dom";
 export default function NavBar() {
     return (
         <nav className="nav">
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import "./main.scss"
 import { BrowserRouter } from 'react-router-dom'
-import './fonts/SF-pro.ttf'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
