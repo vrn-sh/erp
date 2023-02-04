8a53a7d7df8434b103bce807dd73b266eaa133b9
claraxuxu <claraxu1201@gmail.com>
2023-02-04 13:51:03 +0800

    reuseable components

---
+++ web/src/component/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+import "./footer.scss"
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <h2 className="site-title">voron</h2>
+            <p>Copyright ©2023 VORON, Inc.</p>
+            <p>Contact voron@djnn.sh</p>
+            <div className="policy-link">
+                <ul>
+                    <CustomLink to="/">Legal Stuff</CustomLink>
+                    <CustomLink to="/">Privacy Policy</CustomLink>
+                    <CustomLink to="/">TeSecurityam</CustomLink>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+function CustomLink({ to, children, ...props }: {to: string; children: string}) {
+    return (
+        <li>
+            <a href={to} {...props}>
+                {children}
+            </a>
+        </li>
+    ) 
+}
