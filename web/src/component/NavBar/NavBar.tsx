21803eba80848731003caa03895b047cb55749db
Clara <56248408+claraxuxu@users.noreply.github.com>
2023-02-09 16:33:39 +0800

    feat(web): landing page

---
+++ web/src/component/NavBar/NavBar.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import "./NavBar.scss"
+
+export default function NavBar() {
+    return (
+        <nav className="nav">
+            <Link to="#home" className="site-title">voron</Link>
+            <ul>
+                <div className="nav-left">
+                    <CustomLink to="#home">Home</CustomLink>
+                    <CustomLink to="#about">About</CustomLink>
+                    <CustomLink to="#team">Team</CustomLink>
+                </div>
+                <div className="nav-right">
+                    <Link to="/signup">Sign up</Link>
+                    <li className="login">
+                        <Link to="/Login">
+                            Log in
+                        </Link>
+                    </li>
+                </div>
+            </ul>
+        </nav>
+    )
+}
+
+function CustomLink({ to, children, ...props }: {to: string; children: string}) {
+    return (
+        <li>
+            <a href={to} {...props}>
+                {children}
+            </a>
+        </li>
+    ) 
+}
---
+++ web/src/pages/Home/Team/Team.tsx
@@ -0,0 +1,35 @@
+import "./Team.scss"
+
+export default function Team() {
+    return (
+        <div id="team" className="team">
+            <h1>Voron<span>Team</span></h1>
+            <div className="member">
+                <div className="grid">
+                    <MemberBox role="Front-end developper">Camelia</MemberBox>
+                    <MemberBox role="Front-end developper">Blacky</MemberBox>
+                    <MemberBox role="Front-end developper">Clara</MemberBox>
+                </div>
+                <div className="grid">
+                    <MemberBox role="Back-end developper">Djnn</MemberBox>
+                    <MemberBox role="Back-end developper">Naadi</MemberBox>
+                    <MemberBox role="Back-end developper">Mat</MemberBox>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function MemberBox({ children, role, ...props }: {children: string; role: string}) {
+    return (
+        <div className="member-card">
+            <div className="member-content">
+                <div className="photo" />
+                <div className="info-box">
+                    <h4 className="name">{children}</h4>
+                    <h5 className="role">{role}</h5>
+                </div>
+            </div>
+        </div>
+    ) 
+}
