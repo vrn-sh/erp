178424153cf14f202191cd5ce957fa309df90bdf
Nathan <44578736+TheOrionsB@users.noreply.github.com>
2023-02-09 21:36:23 +0100

    fix(web): conflict trash chars removed sry

--- web/src/pages/Home/About/About.tsx
+++ web/src/pages/Home/About/About.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./About.scss";
 import strings from "../../../assets/strings/en/about.json";
 export default function About() {
@@ -29,33 +28,3 @@ export default function About() {
     </div>
   );
 }
-=======
-import "./About.scss"
-
-export default function About() {
-    return (
-        <div id="about" className="about">
-            <h1>About<span>VORON</span></h1>
-            <div>
-                <p>
-                    Voron is a platform that allows service providers 
-                    to test deployed defenses and processes to ensure 
-                    the sustainability of the system, through the missions 
-                    to infiltrate corporate networks which are provided by 
-                    Voron.
-                </p>
-                <p>
-                    Voron ensures that the companies who will be tested 
-                    had already authorized computer attacks on their 
-                    information networks.
-                </p>
-                <p>
-                    Volon will provide a report detailing the findings 
-                    and various vulnerabilities, while attesting that 
-                    the rules of engagement are being followed.
-                </p>
-            </div>
-        </div>
-    )
-}
->>>>>>> a86432b2972997a9c86389e10e5dc904724ee361
