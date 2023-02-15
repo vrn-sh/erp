604a17ced16350296046b9078590dfdfe9f976fb
Nathan <44578736+TheOrionsB@users.noreply.github.com>
2023-02-15 16:29:46 +0100

    feat(web): Unit testing folder add, vitest add to dependencies, placeholder home test running

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/__test__/setup.ts',
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+        },
+    },
+});
