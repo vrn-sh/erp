914f41181d562d256787d638dad3a25ea6e920b7
claraxuxu <claraxu1201@gmail.com>
2023-02-16 13:57:22 +0800

    + basic sidebar structure and styles

---
+++ web/src/component/SideBar/SideBar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import "./SideBar.scss"
+import { SideBarData } from './SideBarData';
+import SubMenu
+ from './SubMenu';
+export default function SideBar() {
+    return (
+        <>
+            <div className='top-bar'>
+                <Link to="#">
+                    <h1 className='site-title'>voron</h1>
+                </Link>
+            </div>
+            <div className='sidebar'>
+                <div className='sidebar-container'>
+                    {SideBarData.map((item, index) => {
+                        return <SubMenu item={item} key={index} />
+                    })}
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ web/src/component/SideBar/SubMenu.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import "./SideBar.scss"
+
+const SubMenu = ({ item }) => {
+    const [ subnav, setSubnav ] = useState(false)
+    const showSubnav = () => setSubnav(!subnav)
+
+    return (
+        <>
+            <Link to={item.path} className="sidebar-link" onClick={item.subNav && showSubnav}>
+                <div className='sidebar-item'>
+                    {item.icon}
+                    <span className='menu-txt'>{item.title}</span>
+                </div>
+                <div>
+                    {item.subNav && subnav 
+                    ? item.iconOpened 
+                    : item.subNav 
+                    ? item.iconClosed 
+                    : null}
+                </div>
+            </Link>
+            {subnav && item.subNav.map((subItem, subIndex) => {
+                return (
+                    <Link to={subItem.path} key={subIndex} className='dropdown-menu'>
+                        <p>{subItem.title}</p>
+                    </Link>
+                )
+            })}
+        </>
+    )
+};
+
+export default SubMenu;
