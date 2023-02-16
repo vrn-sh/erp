efd24c84c6510420c9176dd17b7704d7d223cbb2
claraxuxu <claraxu1201@gmail.com>
2023-02-16 13:58:53 +0800

    del useless lib in import

--- web/src/component/SideBar/SideBar.tsx
+++ web/src/component/SideBar/SideBar.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import "./SideBar.scss"
 import { SideBarData } from './SideBarData';
-import SubMenu
- from './SubMenu';
+import SubMenu from './SubMenu';
+
 export default function SideBar() {
     return (
         <>
