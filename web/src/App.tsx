f1f4fe8750adf1c1c4ea3fe9251691410e4db8cb
claraxuxu <claraxu1201@gmail.com>
2023-02-16 13:56:04 +0800

    +basic routes and lib

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,7 @@ import About from "./pages/Home/About/About"
 import "./global-variable.scss"
 import Login from "./pages/Login/Login"
 import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard/Dashboard"
 
 function App() {  
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/about" element={<About />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
         </Routes>
       </div>
     </>
