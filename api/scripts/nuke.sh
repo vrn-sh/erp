d59478be93391e1b1de28c3a72661132403e1a5e
winds0r <wontsleep@protonmail.com>
2023-02-23 20:58:18 +0900

    fix(api): Access to Redoc documentation on development

--- api/scripts/nuke.sh
+++ api/scripts/nuke.sh
@@ -3,6 +3,6 @@
 echo "This script will destroy the database. Are you sure you want to delete this database ?"
 read -rp "Press [Enter] key to continue..."
 
-export DATABASE="core"
+export DATABASE="voron"
 sudo -u postgres psql -c "DROP DATABASE IF EXISTS ${DATABASE};"
 sudo -u postgres psql -c "DROP DATABASE IF EXISTS ${DATABASE}_test;"
