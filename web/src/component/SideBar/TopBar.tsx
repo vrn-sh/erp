2f561548b98c078253e301f55f5f090637ec01dc
claraxuxu <claraxu1201@gmail.com>
2023-03-02 20:57:08 +0900

    added popup for "add pentester"

--- web/src/component/SideBar/TopBar.tsx
+++ web/src/component/SideBar/TopBar.tsx
@@ -1,29 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SideBar.scss';
 import * as BiIcons from 'react-icons/bi';
 import * as FaIcons from 'react-icons/fa';
 
 export default function TopBar() {
+    const [popup, setPopup] = useState(false);
+
+    const popupClick = () => {
+        setPopup(!popup);
+    };
+
     return (
-        <div className="top-bar">
-            <div className="topBar-wrapper">
-                <div className="topBar-searchIcon">
-                    <BiIcons.BiSearch color="#8A8A8A" />
+        <>
+            <div className="top-bar">
+                <div className="topBar-wrapper">
+                    <div className="topBar-searchIcon">
+                        <BiIcons.BiSearch color="#8A8A8A" />
+                    </div>
+                    <input
+                        className="topBar-input"
+                        type="text"
+                        placeholder="Search"
+                    />
                 </div>
-                <input
-                    className="topBar-input"
-                    type="text"
-                    placeholder="Search"
-                />
+                <div className="btn-left">
+                    <input
+                        type="button"
+                        value="Add pentester"
+                        className="borderBtn"
+                        onClick={popupClick}
+                    />
+                    <span className="btn">
+                        <FaIcons.FaBell size="22px" color="#8A8A8A" />
+                    </span>
+                    <span className="btn-profile">
+                        <FaIcons.FaUserCircle size="22px" color="#8A8A8A" />
+                    </span>
+                </div>
             </div>
-            <div className="btn-left">
-                <span className="btn">
-                    <FaIcons.FaBell size="22px" color="#8A8A8A" />
-                </span>
-                <span className="btn-profile">
-                    <FaIcons.FaUserCircle size="22px" color="#8A8A8A" />
-                </span>
-            </div>
-        </div>
+
+            {popup && (
+                <div className="popup">
+                    <div className="popup-overlay">
+                        <h1>Invite a pentester</h1>
+                        <input
+                            type="text"
+                            placeholder="Email"
+                            className="popup-input"
+                        />
+                        <button type="button" className="sendBtn">
+                            Send invitation
+                        </button>
+                        <button
+                            type="button"
+                            className="sendBtn"
+                            onClick={popupClick}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
--- web/src/pages/Dashboard/DashboardMission.tsx
+++ web/src/pages/Dashboard/DashboardMission.tsx
@@ -8,56 +8,81 @@ type Props = {
 
 function DashboardMission(props: Props) {
     const { list } = props;
-    const [active, setActive] = useState('');
+    const [active, setActive] = useState('1');
+    const [finish] = useState(50);
 
     const handleClick = (event: any) => {
         setActive(event.target.id);
     };
 
+    // const updatePercent = (n: number) => {
+    //     setFinish(n);
+    // }; // need to add setFinish on line 12
+
     return (
         <div className="dashboard-table">
-            <div className="subHeader">
-                <button
-                    key={1}
-                    id="1"
-                    type="button"
-                    className={active === '1' ? 'active' : undefined}
-                    onClick={handleClick}
-                >
-                    My Mission
-                </button>
-                <button
-                    key={2}
-                    id="2"
-                    type="button"
-                    className={active === '2' ? 'active' : undefined}
-                    onClick={handleClick}
-                >
-                    My Roles
-                </button>
+            <h1>Assigned missions</h1>
+            <div className="dashboard-content">
+                <div className="subHeader">
+                    <button
+                        key={1}
+                        id="1"
+                        type="button"
+                        className={active === '1' ? 'active' : undefined}
+                        onClick={handleClick}
+                    >
+                        My Mission
+                    </button>
+                    <button
+                        key={2}
+                        id="2"
+                        type="button"
+                        className={active === '2' ? 'active' : undefined}
+                        onClick={handleClick}
+                    >
+                        My Roles
+                    </button>
+                </div>
+                <table>
+                    {list.map((mission) => {
+                        return (
+                            <tr key={mission.id}>
+                                <td>{mission.name}</td>
+                                <td>{mission.type}</td>
+                                <td>
+                                    <input
+                                        type="button"
+                                        value="Open"
+                                        className="openBtn"
+                                    />
+                                    <input
+                                        type="button"
+                                        value="Edit"
+                                        className="borderBtn"
+                                    />
+                                </td>
+                                {/* Process bar */}
+                                <td>
+                                    <div className="process-td">
+                                        <div className="process-bar">
+                                            <div
+                                                className="process-container"
+                                                style={{
+                                                    width: `${finish}%`,
+                                                    transition: 'width 0.5s',
+                                                }}
+                                            />
+                                        </div>
+                                        <span className="process-percent">
+                                            {finish}%
+                                        </span>
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </table>
             </div>
-            <table>
-                {list.map((mission) => {
-                    return (
-                        <tr key={mission.id}>
-                            <td>{mission.name}</td>
-                            <td>{mission.type}</td>
-                            <td>
-                                <input
-                                    type="button"
-                                    value="Open"
-                                    className="openBtn"
-                                />
-                                <input
-                                    type="button"
-                                    value="Edit"
-                                    className="editBtn"
-                                />
-                            </td>
-                        </tr>
-                    );
-                })}
-            </table>
         </div>
     );
 }
