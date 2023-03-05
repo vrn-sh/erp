14d619e8706a59877fa73408276d019fd54a271a
claraxuxu <claraxu1201@gmail.com>
2023-03-05 22:01:07 +0900

    pagination done, updating sidebar items

--- web/src/component/SideBar/SideBarData.tsx
+++ web/src/component/SideBar/SideBarData.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import * as AiIcons from 'react-icons/ai';
 import * as BsIcons from 'react-icons/bs';
-import * as TbIcons from 'react-icons/tb';
-import * as RiIcons from 'react-icons/ri';
 import * as IoIcons from 'react-icons/io';
 
 const SideBarData = [
@@ -20,40 +18,6 @@ const SideBarData = [
         ],
     },
     {
-        title: 'Client',
-        path: '/dashboard', // change when build client page
-        icon: <BsIcons.BsPeople />,
-        iconClosed: <IoIcons.IoIosArrowForward />,
-        iconOpened: <IoIcons.IoIosArrowDown />,
-        subNav: [
-            {
-                title: 'List item',
-                path: '/dashboard', // can be changed later
-            },
-        ],
-    },
-    {
-        title: 'Hosts',
-        path: '/dashboard', // change when build client page
-        icon: <RiIcons.RiFileList2Line />,
-        iconClosed: <IoIcons.IoIosArrowForward />,
-        iconOpened: <IoIcons.IoIosArrowDown />,
-        subNav: [
-            {
-                title: 'List item',
-                path: '/dashboard', // can be changed later
-            },
-        ],
-    },
-    {
-        title: 'Impacts',
-        path: '/dashboard', // change when build client page
-        icon: <AiIcons.AiOutlineInbox />,
-        iconClosed: <IoIcons.IoIosArrowForward />,
-        iconOpened: <IoIcons.IoIosArrowDown />,
-        subNav: [],
-    },
-    {
         title: 'Missions',
         path: '/dashboard', // change when build client page
         icon: <AiIcons.AiOutlineStar />,
@@ -62,17 +26,9 @@ const SideBarData = [
         subNav: [],
     },
     {
-        title: 'Users',
+        title: 'Create a team',
         path: '/dashboard', // change when build client page
-        icon: <RiIcons.RiShieldUserLine />,
-        iconClosed: <IoIcons.IoIosArrowForward />,
-        iconOpened: <IoIcons.IoIosArrowDown />,
-        subNav: [],
-    },
-    {
-        title: 'Vulnerabilities',
-        path: '/dashboard', // change when build client page
-        icon: <TbIcons.TbTarget />,
+        icon: <BsIcons.BsPeople />,
         iconClosed: <IoIcons.IoIosArrowForward />,
         iconOpened: <IoIcons.IoIosArrowDown />,
         subNav: [],
