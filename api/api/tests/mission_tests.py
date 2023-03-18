b5db370b06eea2f17e188597f725d4f8c5b9cc18
winds0r <wontsleep@protonmail.com>
2023-03-18 23:34:16 +0900

    fix(api): adding Team to Mission model

--- api/api/tests/mission_tests.py
+++ api/api/tests/mission_tests.py
@@ -1,30 +1,45 @@
 from warnings import warn
 from django.test import TransactionTestCase
+from faker import Faker
 
 from rest_framework.test import APIClient
-from api.models import Admin, Pentester, Auth
+from api.models import Manager, Pentester, Auth
 
-from api.tests.helpers import create_random_pentester, create_random_admin, random_user_password, login_as
+from api.tests.helpers import create_random_pentester, create_random_manager, random_user_password, login_as
 
 
 class NotesTestCase(TransactionTestCase):
 
     def setUp(self) -> None:
         self.user: Pentester = create_random_pentester()
-        self.admin: Admin = create_random_admin()
+        self.manager: Manager = create_random_manager()
 
     def tearDown(self) -> None:
         self.user.delete()
-        self.admin.delete()
+        self.manager.delete()
 
     def test_create_and_update_valid_case(self) -> None:
         """
-            logged in user can create a Note
+            logged in user can create a Mission, update it then delete it
         """
         client: APIClient = APIClient()
 
-        auth_token: str = login_as(self.admin.auth.email, random_user_password())
+        auth_token: str = login_as(self.manager.auth.email, random_user_password())
         client.credentials(HTTP_AUTHORIZATION=f'Token {auth_token}')
+
+        # preparing fake data
+        fake_name: str = Faker().name()
+
+        # creating a team
+        data: dict[str, str] = {
+                'name': fake_name,
+                'members': [self.user.id]
+        }
+        resp = client.post('/team', format='json', data=data)
+        self.assertEqual(resp.status_code, 201)
+
+        team_id = resp.data['id']
+
 
         response = client.post(
             "/mission",
@@ -34,9 +49,9 @@ class NotesTestCase(TransactionTestCase):
                 'duration': '400.5',
                 'start': '2022-01-01',
                 'end': '2024-01-01',
+                'team': team_id,
             }
         )
-        warn(f'resposne: {response.content}')
         self.assertEqual(response.status_code, 201)
         id: str = response.data['id']
         response = client.patch(
@@ -49,5 +64,4 @@ class NotesTestCase(TransactionTestCase):
         response = client.delete(
             f"/mission/{id}",
         )
-        warn(f'resposne: {response.status_code}, {response.content}')
         self.assertEqual(response.status_code, 204)
