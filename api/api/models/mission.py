13aef48a16af1a9fb1c0aa6a77a2a2738dc41185
adina <pirouettecacahuete@protonmail.com>
2023-03-18 16:46:59 +0900

    fix(pr): feedback added.

--- api/api/models/mission.py
+++ api/api/models/mission.py
@@ -12,10 +12,18 @@ from api.models import Auth, MAX_TITLE_LENGTH
 # TODO: add a Team one model will be available
 # Create role Manager ??
 class Mission(models.Model):
+
+    class Meta:
+        verbose_name = "Mission Model"
+        verbose_name_plural = "Mission models"
+        ordering = ['start']
+
+    REQUIRED_FIELDS = ['duration', 'start', 'end']
+
     # Duration is in number of days.
     duration = models.FloatField(default=1)
-    start = models.DateField(auto_now_add=True)
-    end = models.DateField(auto_now_add=True)
+    start = models.DateField()
+    end = models.DateField()
     creation_date: models.DateTimeField = models.DateTimeField(auto_now_add=True, editable=False)
     last_updated_date: models.DateTimeField = models.DateTimeField(auto_now_add=True, editable=True)
     created_by = models.ForeignKey(Auth, on_delete=models.CASCADE, related_name='created_by')
--- api/api/serializers/mission.py
+++ api/api/serializers/mission.py
@@ -5,8 +5,5 @@ from api.models.mission import Mission
 
 class MissionSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = [
-            'id', 'start', 'end', 'duration', 'creation_date', 'last_updated_date', 'created_by', 'last_updated_by',
-            'title'
-        ]
+        fields = '__all__'
         model = Mission
