f2c9fbbb208e870114cadcc00f28ff7b76b08bbd
anne <anne.mboke@epitech.eu>
2023-04-13 00:55:08 +0200

    fix(web): fix cards rendering

--- web/src/pages/Dashboard/SubDashboards/Notes.tsx
+++ web/src/pages/Dashboard/SubDashboards/Notes.tsx
@@ -2,41 +2,47 @@ import React, { useState } from 'react';
 import * as IoIcons from 'react-icons/io';
 import { IDashboardNotes, DashboardNotesList } from '../DashBoardNote.type';
 import '../Dashboard.scss';
-// import '../../../component/SideBar/SideBar.scss';
 
-function ViewNote(props: {
+interface ViewNoteProps {
     note: IDashboardNotes;
     func: React.MouseEventHandler<HTMLAnchorElement>;
-}) {
+}
+
+interface AddNoteProps {
+    func: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+function ViewNote({ note, func }: ViewNoteProps) {
     return (
         <div className="modal-wrapper">
             <div className="modal-card">
-            <div className='modal'>
-                <div className="modal-header">
-                         <h2 className="heading">{props.note.title}</h2>
-                         <a
-                            onClick={props.func}
+                <div className="modal">
+                    <div className="modal-header">
+                        <h2 className="heading">{note.title}</h2>
+                        <a
+                            onClick={func}
+                            onKeyDown={() => func}
                             role="button"
                             className="close"
                             aria-label="close this modal"
+                            tabIndex={0}
                         >
                             <svg viewBox="0 0 24 24">
                                 <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
                             </svg>
                         </a>
                     </div>
-                    <p>{props.note.content}</p>
+                    <p>{note.content}</p>
                 </div>
-                <a className="outside-trigger"></a>
             </div>
         </div>
     );
 }
 
-function AddNote(props: { func: React.MouseEventHandler<HTMLButtonElement> }) {
+function AddNote({ func }: AddNoteProps) {
     const [name, setName] = useState('');
     const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+    const content = '';
 
     const takeName = (event: React.ChangeEvent<HTMLInputElement>) => {
         setName(event.target.value);
@@ -46,68 +52,63 @@ function AddNote(props: { func: React.MouseEventHandler<HTMLButtonElement> }) {
         setTitle(event.target.value);
     };
 
-    const takeContent = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setContent(event.target.value);
-    };
-
-    const handleSubmit = () => {
+    const handleSubmit = (
+        evt: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
         const note: IDashboardNotes = {
             id: DashboardNotesList.length.toString(),
-            title: title,
-            content: content,
+            title,
+            content,
             author: name,
         };
-        console.log(note);
         DashboardNotesList.unshift(note);
-        console.log(DashboardNotesList);
-        props.func();
+        func(evt);
     };
 
     return (
         <div className="modal-wrapper">
             <div className="modal-card ">
-                <div className='modal centered'>
-                <h2 className="heading">Add a note</h2>
-                <input
-                    type="text"
-                    required
-                    placeholder="Enter a title"
-                    className="popup-input"
-                    onChange={takeTitlee}
-                    value={title}
-                />
-                <input
-                    required
-                    type="text"
-                    placeholder="Enter your name"
-                    className="popup-input"
-                    onChange={takeName}
-                    value={name}
-                />
-                <textarea
-                    rows={8}
-                    required
-                    placeholder="Put your note here"
-                    className="popup-textarea"
-                    // onChange={takeContent}
-                    value={content}
-                />
-                <div className="btn-block">
-                    <button
-                        type="button"
-                        className="btn cancel"
-                        onClick={props.func}
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        type="button"
-                        className="btn submit"
-                        onClick={handleSubmit}
-                    >
-                        Submit
-                    </button>
-                </div>
+                <div className="modal centered">
+                    <h2 className="heading">Add a note</h2>
+                    <input
+                        type="text"
+                        required
+                        placeholder="Enter a title"
+                        className="popup-input"
+                        onChange={takeTitlee}
+                        value={title}
+                    />
+                    <input
+                        required
+                        type="text"
+                        placeholder="Enter your name"
+                        className="popup-input"
+                        onChange={takeName}
+                        value={name}
+                    />
+                    <textarea
+                        rows={8}
+                        required
+                        placeholder="Put your note here"
+                        className="popup-textarea"
+                        value={content}
+                    />
+                    <div className="btn-block">
+                        <button
+                            type="button"
+                            className="btn cancel"
+                            onClick={() => func}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type="button"
+                            className="btn submit"
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -117,62 +118,72 @@ function AddNote(props: { func: React.MouseEventHandler<HTMLButtonElement> }) {
 export default function Notes() {
     const [list] = useState(DashboardNotesList as IDashboardNotes[]);
     const [modal, setModal] = useState(false);
-    const [isView, setisView] = useState(false);
+    const [displayed, setDisplayed] = useState(-1);
 
     const modalClick = () => {
         setModal(!modal);
     };
 
-    const viewClick = () => {
-        setisView(!isView);
+    const viewClick = (idx: number) => {
+        if (displayed === -1) {
+            setDisplayed(idx);
+        } else {
+            setDisplayed(-1);
+        }
     };
 
     return (
-        <>
-            <div className="container-note cards">
-                <div className="card">
-                    <div>
-                        <h2 className="heading">Add a note</h2>
-                    </div>
-                    <footer>
-                        <a
-                            onClick={modalClick}
-                            role="button"
-                            className="button__link"
-                        >
-                            <IoIcons.IoIosAdd
-                                size={'90px'}
-                                color="rebeccapurple"
-                            />
-                        </a>
-                    </footer>
+        <div className="container-note cards">
+            <div className="card">
+                <div>
+                    <h2 className="heading">Add a note</h2>
                 </div>
-                {modal && <AddNote func={modalClick} />}
-                {list.map((notes, index) => {
-                    return (
-                        <div className="card" key={notes.id} id={index.toString()}>
-                            <div>
-                                <h2 className="heading">{notes.title}</h2>
-                                <p className="card-content">{notes.content}</p>
-                            </div>
-                            <footer>
-                                {notes.author}
-                                <a
-                                    role="button"
-                                    className="button__link"
-                                    onClick={viewClick}
-                                    color='rebeccapurple'
-                                >
-                                    View more
-                                </a>
-                            </footer>
-                            {isView && (
-                                <ViewNote note={notes} func={viewClick} />
-                            )}
-                        </div>
-                    );
-                })}
+                <footer>
+                    <a
+                        onClick={modalClick}
+                        onKeyDown={modalClick}
+                        tabIndex={0}
+                        role="button"
+                        className="button__link"
+                    >
+                        <IoIcons.IoIosAdd size="90px" color="rebeccapurple" />
+                    </a>
+                </footer>
             </div>
-        </>
+            {modal && <AddNote func={modalClick} />}
+            {list.map((notes, index) => {
+                return (
+                    <div className="card" key={`component-${notes.id}`}>
+                        <div>
+                            <h2 className="heading">{notes.title}</h2>
+                            <p className="card-content">{notes.content}</p>
+                        </div>
+                        <footer>
+                            {notes.author}
+                            <a
+                                role="button"
+                                className="button__link"
+                                onKeyDown={() => {
+                                    viewClick(index);
+                                }}
+                                tabIndex={0}
+                                onClick={() => {
+                                    viewClick(index);
+                                }}
+                                color="rebeccapurple"
+                            >
+                                View more
+                            </a>
+                        </footer>
+                        {displayed === index && (
+                            <ViewNote
+                                note={notes}
+                                func={() => viewClick(index)}
+                            />
+                        )}
+                    </div>
+                );
+            })}
+        </div>
     );
 }
