9129f3411aca3ef623376fc35694c042f696644f
Adina <59285665+lambdina@users.noreply.github.com>
2023-04-13 17:02:16 +0900

    feat(api): fixing documentation endpoint (#110)
    * fix(ci): deployment workflow should only run if all others are successful (#107)
    
    * feat(web): cosmetics (#106)
    
    Co-authored-by: djnn | 진 <wontsleep@protonmail.com>
    Co-authored-by: clara mac <claraxu1201@gmail.com>
    
    * feat(api): adding `Nmap` CRUD (#109)
    
    * WIP: adding custom NmapPort Field & model for nmap crud
    * feat(api): adding `NmapScan` model
    * fix(api): unit tests for `NmapScan` creation
    * feat(api): Adding permissions middleware for `NmapScan` + tests
    
    * feat(docs): add response samples + optional fields
    
    * fix(docs): precise security auth
    
    * fix(api): add nmap_file and request default sample
    
    * fix(api)
    
    ---------
    
    Co-authored-by: djnn | 진 <wontsleep@protonmail.com>
    Co-authored-by: Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
    Co-authored-by: Loris-Reggiani <71264335+Loris-Reggiani@users.noreply.github.com>
    Co-authored-by: clara mac <claraxu1201@gmail.com>

--- api/api/views/viewsets/mission.py
+++ api/api/views/viewsets/mission.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 from warnings import warn
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, permissions
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.routers import Response
@@ -8,7 +11,7 @@ from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from api.models.mission import Mission, NmapScan, Recon
 from api.permissions import IsManager, IsOwner, IsPentester, ReadOnly
 from api.serializers.mission import MissionSerializer, NmapSerializer, ReconSerializer
-from api.models.utils import parse_nmap_ips, parse_nmap_domain, parse_nmap_scan
+from api.models.utils import parse_nmap_ips, parse_nmap_domain, parse_nmap_scan, default_nmap_output
 
 
 class NmapViewset(viewsets.ModelViewSet):
@@ -20,11 +23,37 @@ class NmapViewset(viewsets.ModelViewSet):
     serializer_class = NmapSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner, IsManager & ReadOnly | IsPentester]
 
+    @swagger_auto_schema(
+        operation_description="Creates and parses an NMAP output object.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['recon_id', 'nmap_file'],
+            properties={
+                'recon_id': openapi.Schema(type=openapi.TYPE_INTEGER,
+                                           description="Id of recon"),
+                'nmap_file': openapi.Schema(type=openapi.TYPE_STRING,
+                                      description="Nmap output.", default=default_nmap_output),
+            },
+        ),
+        responses={
+            "200": openapi.Response(
+                description="200 OK",
+                examples={
+                    "id": 1,
+                    "recon_id": 4,
+                    "ips": ["127.0.0.1", "0.0.0.0"],
+                    "ports": ["8080,http,up,django"]
+                }
+            )
+        },
+        security=['Bearer'],
+        tags=['NMAP'],
+    )
     def create(self, request, *args, **kwargs):
         fields = {
-                'ips': parse_nmap_ips,
-                'domain': parse_nmap_domain,
-                'ports': parse_nmap_scan,
+            'ips': parse_nmap_ips,
+            'domain': parse_nmap_domain,
+            'ports': parse_nmap_scan,
         }
 
         for field, func in fields.items():
@@ -48,9 +77,9 @@ class NmapViewset(viewsets.ModelViewSet):
         instance = self.get_object()
 
         fields = {
-                'ips': parse_nmap_ips,
-                'domain': parse_nmap_domain,
-                'ports': parse_nmap_scan,
+            'ips': parse_nmap_ips,
+            'domain': parse_nmap_domain,
+            'ports': parse_nmap_scan,
         }
 
         data = {}
@@ -70,7 +99,7 @@ class NmapViewset(viewsets.ModelViewSet):
         return Response(self.get_serializer(instance).data)
 
 
-class ReconViewset(viewsets.ModelViewSet): # pylint: disable=too-many-ancestors
+class ReconViewset(viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
     """
         CRUD for Recon object
     """
@@ -87,10 +116,41 @@ class MissionViewset(viewsets.ModelViewSet):  # pylint: disable=too-many-ancesto
     """
 
     queryset = Mission.objects.all()
-    permission_classes = [permissions.IsAuthenticated,  IsOwner, IsPentester & ReadOnly | IsManager]
+    permission_classes = [permissions.IsAuthenticated, IsOwner, IsPentester & ReadOnly | IsManager]
     authentication_classes = [TokenAuthentication]
     serializer_class = MissionSerializer
 
+    @swagger_auto_schema(
+        operation_description="Creates a mission. Must be done by a Manager.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['start', 'end', 'team'],
+            properties={
+                'start': openapi.Schema(type=openapi.FORMAT_DATE,
+                                        description="date of mission start"),
+                'end': openapi.Schema(type=openapi.FORMAT_DATE,
+                                      description="date of mission ends"),
+                'title': openapi.Schema(type=openapi.TYPE_STRING,
+                                        description="Title of the mission"),
+                'team': openapi.Schema(type=openapi.TYPE_INTEGER,
+                                       description="Id of the team")
+            },
+        ),
+        responses={
+            "200": openapi.Response(
+                description="200 OK",
+                examples={
+                    "id": 1,
+                    "title": "Pentest Epitech",
+                    "start": "2020-06-03",
+                    "end": "2022-06-03",
+                    "team": 2
+                }
+            )
+        },
+        security=['Bearer'],
+        tags=['mission'],
+    )
     def create(self, request, *args, **kwargs):
         request.data['created_by'] = request.user.id
         request.data['last_updated_by'] = request.user.id
@@ -98,7 +158,6 @@ class MissionViewset(viewsets.ModelViewSet):  # pylint: disable=too-many-ancesto
         return super().create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-
         if "created_by" in request.data:
             request.data.pop("created_by")
 
