a111f5566567fca49280108ca77f392e29372be2
djnn | 진 <wontsleep@protonmail.com>
2023-04-16 18:48:40 +0900

    feat(web): Create/Edit mission UI (#117)
    ---------
    
    Co-authored-by: samacamelia <camelia.sama@epitech.eu>
    Co-authored-by: clara mac <claraxu1201@gmail.com>

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,10 +4,13 @@ import Home from './pages/Home/Home';
 import About from './pages/Home/About/About';
 import './global-variable.scss';
 import Login from './pages/Login/Login';
+import ProfilePage from './pages/Profile/Profile';
+import EditMission from './pages/EditMission/EditMission';
+import CreateMission from './pages/EditMission/CreateMission';
 import Dashboard from './pages/Dashboard/Dashboard';
 import SignUp from './pages/SignUp/SignUp';
 import NotFound from './pages/Error/Error';
-import Setting from './pages/Settings/Settings'; // eslint-disable-line
+import Setting from './pages/Settings/Settings';
 
 function App() {
     return (
@@ -16,9 +19,12 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
+                <Route path="/sign_up" element={<SignUp />} />
                 <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/setting" element={<Setting />} />
+                <Route path="/settings" element={<Setting />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/edit_mission" element={<EditMission />} />
+                <Route path="/create_mission" element={<CreateMission />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
