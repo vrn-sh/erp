17337182dd3a5c88ab404044bba827d555fbb83f
winds0r <wontsleep@protonmail.com>
2023-04-16 19:13:49 +0900

    fix(web): removing lorem-ipsum on login page

--- web/src/pages/Login/Login.tsx
+++ web/src/pages/Login/Login.tsx
@@ -74,21 +74,12 @@ export default function Login() {
 
     return (
         <section className="login-container">
-            <div className="login-text" id="login-text">
-                <div>
-                    <h2>voron</h2>
-                    <h1>{import.meta.env.VITE_REACT_APP_SLOGAN}</h1>
-                    <span className="no-bold">
-                        Lorem ipsum dolor sit amet consectetur. Quis platea
-                        lectus.
-                    </span>
-                </div>
-            </div>
+            <div className="login-text" id="login-text" />
 
             <div className="login-form" id="login-form">
                 <div className="wrapper">
                     <div className="form-wrapper">
-                        <h2>Welcome back!</h2>
+                        <h2>Welcome back !</h2>
 
                         <div className="form-group">
                             <label>Email</label>
