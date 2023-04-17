b6ceb6b9c6f5bc83d701add5f17dbd81343b6ccb
winds0r <wontsleep@protonmail.com>
2023-04-17 22:27:09 +0900

    fix(web): linter & build

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,13 +5,13 @@ import About from './pages/Home/About/About';
 import './global-variable.scss';
 import Login from './pages/Login/Login';
 import ProfilePage from './pages/Profile/Profile';
-import EditMission from './pages/editMission/EditMission';
-import CreateMission from './pages/editMission/CreateMission';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Setting from './pages/Setting/setting';
-import AddVulnerability from './pages/vulnerability/addvulnerability';
+import AddVulnerability from './pages/Vulnerability/Add';
 import SignUp from './pages/SignUp/SignUp';
 import NotFound from './pages/Error/Error';
+import Settings from './pages/Settings/Settings';
+import EditMission from './pages/EditMission/EditMission';
+import CreateMission from './pages/EditMission/CreateMission';
 
 function App() {
     return (
@@ -22,12 +22,8 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/sign_up" element={<SignUp />} />
                 <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/setting" element={<Setting />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/editmission" element={<EditMission />} />
-                <Route path="/createmission" element={<CreateMission />} />
-                <Route path="/addvulnerability" element={<AddVulnerability />} />
-                <Route path="/settings" element={<Setting />} />
+                <Route path="/vuln/add" element={<AddVulnerability />} />
+                <Route path="/settings" element={<Settings />} />
                 <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/mission/edit" element={<EditMission />} />
                 <Route path="/mission/create" element={<CreateMission />} />
