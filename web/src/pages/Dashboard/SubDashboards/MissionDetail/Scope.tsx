346dbac61db3ebf47276b9843236feb0dcf859b6
clara mac <claraxu1201@gmail.com>
2023-04-21 12:50:30 +0900

    (web) add table

--- web/src/pages/Dashboard/SubDashboards/MissionDetail/Scope.tsx
+++ web/src/pages/Dashboard/SubDashboards/MissionDetail/Scope.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import '../../Dashboard.scss';
 import './MissionDetail.scss';
+import * as AiIcons from 'react-icons/ai';
 
 export default function Scope() {
     return (
         <table className="no_center_container">
             <tbody>
                 <tr>
-                    <td>1</td>
-                    <td>2</td>
+                    <th>Status</th>
+                    <th>Name</th>
+                    <th>Badges</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td style={{ fontSize: '18px' }}>
+                        <AiIcons.AiOutlineCheckCircle />
+                    </td>
+                    <td>https://evil.djnn.sh</td>
+                    <td />
                     <td>
-                        <input type="button" value="Open" className="openBtn" />
                         <input
                             type="button"
                             value="Edit"
                             className="borderBtn"
                         />
+                        <AiIcons.AiFillDelete />
+                        <AiIcons.AiFillEdit />
                     </td>
-                    <td>-</td>
                 </tr>
             </tbody>
         </table>
