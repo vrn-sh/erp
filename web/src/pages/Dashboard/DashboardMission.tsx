4780dfb702274cf68aa7579c19e70310fdfa04ff
Clara <56248408+claraxuxu@users.noreply.github.com>
2023-04-25 18:25:39 +0900

    (web) add mission button (#130)

--- web/src/pages/Dashboard/DashboardMission.tsx
+++ web/src/pages/Dashboard/DashboardMission.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import './Dashboard.scss';
+import { useNavigate } from 'react-router-dom';
 import DorkEngine from './SubDashboards/DorkEngine';
 import Mission from './SubDashboards/Mission';
 import Notes from './SubDashboards/Notes';
 
 function DashboardMission() {
     const [active, setActive] = useState('main');
+    const navigate = useNavigate();
 
     const handleClick = (event: any) => {
         setActive(event.target.id);
@@ -24,6 +26,10 @@ function DashboardMission() {
         return null;
     };
 
+    const addMission = () => {
+        navigate('/mission/create');
+    };
+
     // const updatePercent = (n: number) => {
     //     setFinish(n);
     // }; // need to add setFinish on line 12
@@ -33,41 +39,50 @@ function DashboardMission() {
             <h1>Assigned missions</h1>
             <div className="dashboard-content">
                 <div className="subHeader">
+                    <div className="submenu-mission">
+                        <button
+                            key={1}
+                            id="main"
+                            type="button"
+                            className={active === 'main' ? 'active' : undefined}
+                            onClick={handleClick}
+                        >
+                            Main
+                        </button>
+                        <button
+                            key={2}
+                            id="note"
+                            type="button"
+                            className={active === 'note' ? 'active' : undefined}
+                            onClick={handleClick}
+                        >
+                            Note
+                        </button>
+                        <button
+                            key={3}
+                            id="vuln"
+                            type="button"
+                            className={active === 'vuln' ? 'active' : undefined}
+                            onClick={handleClick}
+                        >
+                            Vulnerability
+                        </button>
+                        <button
+                            key={4}
+                            id="dork"
+                            type="button"
+                            className={active === 'dork' ? 'active' : undefined}
+                            onClick={handleClick}
+                        >
+                            Dork Engine
+                        </button>
+                    </div>
                     <button
-                        key={1}
-                        id="main"
                         type="button"
-                        className={active === 'main' ? 'active' : undefined}
-                        onClick={handleClick}
+                        className="mission_create"
+                        onClick={addMission}
                     >
-                        Main
-                    </button>
-                    <button
-                        key={2}
-                        id="note"
-                        type="button"
-                        className={active === 'note' ? 'active' : undefined}
-                        onClick={handleClick}
-                    >
-                        Note
-                    </button>
-                    <button
-                        key={3}
-                        id="vuln"
-                        type="button"
-                        className={active === 'vuln' ? 'active' : undefined}
-                        onClick={handleClick}
-                    >
-                        Vulnerability
-                    </button>
-                    <button
-                        key={4}
-                        id="dork"
-                        type="button"
-                        className={active === 'dork' ? 'active' : undefined}
-                        onClick={handleClick}
-                    >
-                        Dork Engine
+                        Add mission
                     </button>
                 </div>
                 {getSubDashboard()}
