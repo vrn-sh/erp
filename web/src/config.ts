a8915eb56ec26b62a6444ce9f5148959e9c3fe7f
winds0r <wontsleep@protonmail.com>
2023-05-05 23:21:56 +0900

    uuuuuuuuu

--- web/src/config.ts
+++ web/src/config.ts
@@ -2,7 +2,6 @@ let apiUrl = 'https://voron.djnn.sh'; // fuck you
 
 if (import.meta.env.VITE_REACT_APP_NODE_ENV === 'development')
     apiUrl = import.meta.env.VITE_REACT_APP_LOCAL_API_URL as string;
-else apiUrl = import.meta.env.VITE_REACT_APP_REMOTE_API_URL as string;
 
 const config = {
     apiUrl,
