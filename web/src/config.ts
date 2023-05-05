7f5cb6557ba56258c482c62f528806abf4a68f68
winds0r <wontsleep@protonmail.com>
2023-05-05 23:26:34 +0900

    xD

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,4 +1,4 @@
-let apiUrl = 'https://voron.djnn.sh'; // fuck you
+let apiUrl = 'https://voron.djnn.sh/api'; // fuck you
 
 if (import.meta.env.VITE_REACT_APP_NODE_ENV === 'development')
     apiUrl = import.meta.env.VITE_REACT_APP_LOCAL_API_URL as string;
