dec3a29379f2e7fa2466629867357fba57e01878
clara mac <claraxu1201@gmail.com>
2023-05-08 16:12:33 +0900

    (web) crt from local result, special pagination with crt+

--- web/src/pages/Dashboard/SubDashboards/CrtSh.tsx
+++ web/src/pages/Dashboard/SubDashboards/CrtSh.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-// import * as IoIcons from 'react-icons/io';
-// import DorkList from '../../../assets/strings/en/dorks.json';
+import toast, { Toaster } from 'react-hot-toast';
+import * as IoIcons from 'react-icons/io';
+import Crt from '../../../assets/strings/en/crt.json';
 
 export default function CrtSh() {
     const [tmpIdentity, setTmpIdentity] = useState('');
@@ -22,33 +23,41 @@ export default function CrtSh() {
         }
     };
 
-    // const [currentPage, setCurrentPage] = useState(1);
-    // const recordsPerPage = 3;
-    // const lastIndex = currentPage * recordsPerPage;
-    // const firstIndex = lastIndex - recordsPerPage;
-    // const records = DorkList.dorks.slice(firstIndex, lastIndex);
-    // const npage = Math.ceil(DorkList.dorks.length / recordsPerPage);
-    // const nums = [...Array(npage + 1).keys()].slice(1);
+    const searchIdentity = () => {
+        setTmpIdentity(tmpIdentity);
+        toast.success('Searching domain');
+    };
 
-    // const nextPage = () => {
-    //     if (currentPage !== npage) {
-    //         setCurrentPage(currentPage + 1);
-    //     }
-    // };
+    const [currentPage, setCurrentPage] = useState(1);
+    const recordsPerPage = 4;
+    const lastIndex = currentPage * recordsPerPage;
+    const firstIndex = lastIndex - recordsPerPage;
+    const records = Crt.slice(firstIndex, lastIndex);
+    const npage = Math.ceil(Crt.length / recordsPerPage);
 
-    // const prePage = () => {
-    //     if (currentPage > 1) {
-    //         setCurrentPage(currentPage - 1);
-    //     }
-    // };
+    const nextPage = () => {
+        if (currentPage !== npage) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
 
-    // const changePage = (n: number) => {
-    //     setCurrentPage(n);
-    // };
+    const prePage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const changePage = (e: string) => {
+        setCurrentPage(parseInt(e, 10));
+    };
 
     return (
         <>
             <div className="dork_input">
+                <div>
+                    <Toaster position="top-center" reverseOrder={false} />
+                </div>
+                <label>Target Identity</label>
                 <input
                     type="text"
                     placeholder="Enter an Identity"
@@ -57,40 +66,74 @@ export default function CrtSh() {
                     onKeyDown={handleKeyDown}
                     className="sub_input_field"
                 />
+                <button
+                    type="button"
+                    onClick={searchIdentity}
+                    className="searchBtn"
+                >
+                    Search
+                </button>
             </div>
-            <table className="no_center_container" />
-            {/* <nav>
+            <table
+                style={{ marginTop: '4px', height: '120px' }}
+                className="no_center_container"
+            >
+                <tbody>
+                    <thead>
+                        <tr>
+                            <th className="md-1">Crt.sh ID</th>
+                            <th className="md-1">Logged At</th>
+                            <th className="md-1">Not Before</th>
+                            <th className="md-1">Not After</th>
+                            <th className="md-1">Name</th>
+                            <th className="md-1">CA ID</th>
+                            <th className="md-3">CA Name</th>
+                        </tr>
+                    </thead>
+                    {records.map((crt) => {
+                        return (
+                            <tbody>
+                                <tr>
+                                    <td>{crt.id}</td>
+                                    <td>{crt.logged_at}</td>
+                                    <td>{crt.not_before}</td>
+                                    <td>{crt.not_after}</td>
+                                    <td>{crt.name}</td>
+                                    <td>{crt.ca.caid}</td>
+                                    <td>{crt.ca.name}</td>
+                                </tr>
+                            </tbody>
+                        );
+                    })}
+                </tbody>
+            </table>
+            <nav>
                 <ul className="pagination">
                     <li className="page-item">
                         <a href="#" className="page-link" onClick={prePage}>
                             <IoIcons.IoIosArrowBack />
                         </a>
                     </li>
-                    {nums.map((n) => {
-                        return (
-                            <li
-                                key={n}
-                                className={`page-item ${
-                                    currentPage === n ? 'active' : ''
-                                }`}
-                            >
-                                <a
-                                    href="#"
-                                    className="page-link"
-                                    onClick={() => changePage(n)}
-                                >
-                                    {n}
-                                </a>
-                            </li>
-                        );
-                    })}
+                    <input
+                        type="number"
+                        key={currentPage}
+                        onChange={(e) => changePage(e.target.value)}
+                        defaultValue={JSON.stringify(currentPage)}
+                        min="1"
+                        className="crt-pagination"
+                        onKeyDown={(event) => {
+                            if (!/[0-9]|Backspace/.test(event.key))
+                                event.preventDefault();
+                        }}
+                    />{' '}
+                    / {npage}
                     <li className="page-item">
                         <a href="#" className="page-link" onClick={nextPage}>
                             <IoIcons.IoIosArrowForward />
                         </a>
                     </li>
                 </ul>
-            </nav> */}
+            </nav>
         </>
     );
 }
