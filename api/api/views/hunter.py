def2a0c30c12a0d8a7b58b31eb8f17a51cb48be5
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-06-21 19:30:21 +0200

    fix(api): update HunterView

--- api/api/views/hunter.py
+++ api/api/views/hunter.py
@@ -12,20 +12,20 @@ class HunterView(APIView):
         first_name = request.query_params.get('first_name', None)
         last_name = request.query_params.get('last_name', None)
 
-        if domain:
+        if first_name and last_name and domain:
+            domain = request.query_params.get('domain', None)
+            hunter = Hunter()
+            email_info = hunter.get_email_finder(first_name, last_name, domain)
+            return Response(email_info, status=status.HTTP_200_OK)
+
+        elif domain:
             hunter = Hunter()
             domain_info = hunter.get_domain(domain)
             return Response(domain_info, status=status.HTTP_200_OK)
 
-        if email:
+        elif email:
             hunter = Hunter()
             email_info = hunter.verify_email(email)
-            return Response(email_info, status=status.HTTP_200_OK)
-
-        if first_name and last_name:
-            domain = request.query_params.get('domain', None)
-            hunter = Hunter()
-            email_info = hunter.get_email_finder(first_name, last_name, domain)
             return Response(email_info, status=status.HTTP_200_OK)
 
         return Response({'message': 'Invalid request'}, status=status.HTTP_400_BAD_REQUEST)
