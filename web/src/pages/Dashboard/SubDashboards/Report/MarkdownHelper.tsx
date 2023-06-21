f6d670dc89b88ab928a6a61199c1580908139637
Adina <59285665+lambdina@users.noreply.github.com>
2023-06-22 01:48:16 +0900

    feat(web): add helper component (#233)

---
+++ web/src/pages/Dashboard/SubDashboards/Report/MarkdownHelper.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import './MarkdownHelper.scss';
+
+export default function MarkdownHelper() {
+    return (
+        <div className="markdown-helper">
+            <ul>
+                <li>
+                    <h1># Header 1</h1>
+                </li>
+                <li>
+                    <p className="help-text">This will generate a h1 tag</p>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h2>## Header 2</h2>
+                </li>
+                <li>
+                    <p className="help-text">This will generate a h2 tag</p>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h6>### Header 6</h6>
+                </li>
+                <li>
+                    <p className="help-text">This will generate a h6 tag</p>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <p className="bold">**bold**</p>
+                </li>
+                <li>
+                    <p className="help-text">
+                        This makes the content between the ** bold
+                    </p>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <p className="italic">*italic*</p>
+                </li>
+                <li>
+                    <p className="help-text">
+                        This makes the content between the * italic
+                    </p>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <ul>
+                        <li className="puce">
+                            <p>something</p>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <p className="help-text">
+                        Putting one * in front of a line makes a list
+                    </p>
+                </li>
+            </ul>
+            <ul>
+                <li>_ _ _</li>
+                <li>
+                    <p className="help-text">
+                        This one is to generate a whole new page
+                    </p>
+                </li>
+            </ul>
+        </div>
+    );
+}
