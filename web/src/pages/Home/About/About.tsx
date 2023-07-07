d27007e34fbc30d75ad52558aa9163e08020a0cd
RisloSS reggiani <loris.reggiani@epitech.eu>
2023-07-07 04:07:30 +0200

    modifying the About section

--- web/src/pages/Home/About/About.tsx
+++ web/src/pages/Home/About/About.tsx
@@ -6,21 +6,6 @@ import strings from '../../../assets/strings/en/about.json';
 export default function About() {
     return (
         <div id="about" className="about">
-            <div className="about-main-row">
-                <Link to="/sign_up" className="about-catch">
-                    <h1>{strings.catch}</h1>
-                    <button type="button">{strings.catchCTA}</button>
-                </Link>
-                <div className="about-bullets">
-                    <p>{strings.catchHighlight}</p>
-                    <ul>
-                        {strings.bullets.map((bullet) => (
-                            <li>{bullet}</li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-
             <div className="about-more">
                 {strings.more.map((item) => (
                     <div>
@@ -29,6 +14,22 @@ export default function About() {
                     </div>
                 ))}
             </div>
+
+            <div className="about-main-row">
+                <Link to="/sign_up" className="about-catch">
+                    <h1>{strings.catch}</h1>
+                    <button type="button">{strings.catchCTA}</button>
+                </Link>
+                {/* <div className="about-bullets">
+                    <p>{strings.catchHighlight}</p>
+                    <ul>
+                        {strings.bullets.map((bullet) => (
+                            <li>{bullet}</li>
+                        ))}
+                    </ul>
+                </div> */}
+            </div>
+
         </div>
     );
 }
