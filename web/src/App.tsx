a04b87d5514cccdcd658684588ab08fd2d6ec849
cameliasmm <camelia.sama@epitech.eu>
2023-09-07 16:53:51 -0700

    done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -21,6 +21,7 @@ import TermOfUse from './pages/Home/TermOfUse';
 import ConfirmEmail from './pages/SignUp/ConfirmEmail';
 import EditVulnerability from './pages/Vulnerability/EditVuln';
 import VulnerabilityDetail from './pages/Vulnerability/VulnDetail';
+import ViewTeamDetails from './pages/Team/ViewTeam';
 
 function App() {
     return (
