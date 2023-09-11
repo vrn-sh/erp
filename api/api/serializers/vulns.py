e98089d30398c9438a8172f22a1b6bf21b308c57
winds0r <wontsleep@protonmail.com>
2023-09-11 21:15:21 +0200

    fix(api): checking if `instance.images` is populated before fetching

--- api/api/serializers/vulns.py
+++ api/api/serializers/vulns.py
@@ -2,9 +2,6 @@ import os
 from typing import List
 
 from rest_framework import serializers
-from io import BytesIO
-import uuid
-from api.serializers.utils import get_image_data, get_mime_type
 
 from api.services.s3 import S3Bucket
 from api.models.vulns import Notes, VulnType, Vulnerability
@@ -32,6 +29,14 @@ class VulnerabilitySerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         s3_client = S3Bucket()
+
+        representation['images'] = []
+
+        if '1' in (os.environ.get('CI', '0'), os.environ.get('TEST', '0')):
+            return representation
+
+        if not instance.images:
+            return representation
 
         images: List[str] = []
         for image in instance.images:
