b0c537cf03dce7a9507bec4a10c33d7398cf92dc
Clara <56248408+claraxuxu@users.noreply.github.com>
2023-09-15 15:40:04 +0200

    feat(web): remove search on file from team creation form (#261)
    * fixed one search bar
    
    * (web) lint fix in this file
    
    * (web) del some useless div and style

--- web/src/component/SideBar/TopBar.tsx
+++ web/src/component/SideBar/TopBar.tsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './SideBar.scss';
-import * as BiIcons from 'react-icons/bi';
 import * as FaIcons from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
 
 export default function TopBar() {
-    const [popup, setPopup] = useState(false);
     const navigate = useNavigate();
-    // const isPentester = Cookies.get('Role') === '1';
-
-    const popupClick = () => {
-        setPopup(!popup);
-    };
-
     const handleKeydown = () => {};
 
     const goProfile = () => {
@@ -22,28 +13,7 @@ export default function TopBar() {
 
     return (
         <div className="top-bar">
-            <div className="topBar-wrapper">
-                {/* <div className="topBar-searchIcon">
-                    <BiIcons.BiSearch color="#8A8A8A" />
-                </div>
-                <input
-                    className="topBar-input"
-                    type="text"
-                    placeholder="Search"
-                /> */}
-            </div>
             <div className="btn-left">
-                {/* {!isPentester && (
-                    <input
-                        type="button"
-                        value="Add pentester"
-                        className="borderBtn"
-                        onClick={popupClick}
-                    />
-                )} */}
-                {/* <span className="btn">
-                    <FaIcons.FaBell size="22px" color="#8A8A8A" />
-                </span> */}
                 <span
                     className="btn-profile"
                     onClick={goProfile}
