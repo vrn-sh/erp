18b7042005eb39987072b61804478d67b483df17
Clara <56248408+claraxuxu@users.noreply.github.com>
2023-09-19 10:48:30 +0200

    feat(web): profile page improvements (#267)
    * (web) profile principal interface done
    
    * (web) profile style ok
    
    * (web)Api + | front fixing
    
    * (web) profile done
    
    * fix(web): fuck eslint
    
    * pr request fix
    
    ---------
    
    Co-authored-by: winds0r <wontsleep@protonmail.com>

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,7 +1,7 @@
-// let apiUrl = 'https://voron.djnn.sh/api'; // fuck you
-const apiUrl = 'http://127.0.0.1:8000';
-// if (import.meta.env.VITE_REACT_APP_NODE_ENV === 'development')
-//     apiUrl = import.meta.env.VITE_REACT_APP_LOCAL_API_URL as string;
+let apiUrl = 'https://voron.djnn.sh/api';
+
+if (import.meta.env.VITE_REACT_APP_NODE_ENV === 'development')
+    apiUrl = import.meta.env.VITE_REACT_APP_LOCAL_API_URL as string;
 
 const config = {
     apiUrl,
