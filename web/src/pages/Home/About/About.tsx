e1d8d3260a03d673a3231148239392cee0ef992b
clara mac <claraxu1201@gmail.com>
2023-09-21 17:49:55 +0200

    scope fixed

--- web/src/pages/Home/About/About.tsx
+++ web/src/pages/Home/About/About.tsx
@@ -7,27 +7,18 @@ export default function About() {
     return (
         <div id="about" className="about">
             <div className="about-main-row">
-                <Link to="/sign_up" className="about-catch">
-                    <h1>{strings.catch}</h1>
-                    <button type="button">{strings.catchCTA}</button>
-                </Link>
-                <div className="about-bullets">
-                    <p>{strings.catchHighlight}</p>
-                    <ul>
-                        {strings.bullets.map((bullet) => (
-                            <li>{bullet}</li>
-                        ))}
-                    </ul>
+                <div className="about-more">
+                    {strings.more.map((item) => (
+                        <div>
+                            <h1 className="title">{item.title}</h1>
+                            <p>{item.desc}</p>
+                        </div>
+                    ))}
+                    <Link to="/sign_up" className="about-catch">
+                        <h1>{strings.catch}</h1>
+                        <button type="button">{strings.catchCTA}</button>
+                    </Link>
                 </div>
-            </div>
-
-            <div className="about-more">
-                {strings.more.map((item) => (
-                    <div>
-                        <h2>{item.title}</h2>
-                        <p>{item.desc}</p>
-                    </div>
-                ))}
             </div>
         </div>
     );
