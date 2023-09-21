36379b19dfe0e72f3661ca8bb29a880a848f5546
winds0r <wontsleep@protonmail.com>
2023-09-21 17:19:20 +0200

    fix(web): linter

--- web/src/pages/Home/About/About.tsx
+++ web/src/pages/Home/About/About.tsx
@@ -7,21 +7,19 @@ export default function About() {
     return (
         <div id="about" className="about">
             <div className="about-main-row">
-            <div className="about-more">
-                {strings.more.map((item) => (
-                    <div>
-                        <h1 className='title'>{item.title}</h1>
-                        <p>{item.desc}</p>
-                    </div>
-                ))}
-                <Link to="/sign_up" className="about-catch">
-                    <h1>{strings.catch}</h1>
-                    <button type="button">{strings.catchCTA}</button>
-                </Link>
+                <div className="about-more">
+                    {strings.more.map((item) => (
+                        <div>
+                            <h1 className="title">{item.title}</h1>
+                            <p>{item.desc}</p>
+                        </div>
+                    ))}
+                    <Link to="/sign_up" className="about-catch">
+                        <h1>{strings.catch}</h1>
+                        <button type="button">{strings.catchCTA}</button>
+                    </Link>
+                </div>
             </div>
-            </div>
-
-
         </div>
     );
 }
