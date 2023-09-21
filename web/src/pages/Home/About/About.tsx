1930cf128e0a1c99c930ffb1d5baede7d1778edf
Loris-Reggiani <71264335+Loris-Reggiani@users.noreply.github.com>
2023-09-21 17:17:05 +0200

    feat(web): changes to About page (#283)

--- web/src/pages/Home/About/About.tsx
+++ web/src/pages/Home/About/About.tsx
@@ -7,28 +7,21 @@ export default function About() {
     return (
         <div id="about" className="about">
             <div className="about-main-row">
+            <div className="about-more">
+                {strings.more.map((item) => (
+                    <div>
+                        <h1 className='title'>{item.title}</h1>
+                        <p>{item.desc}</p>
+                    </div>
+                ))}
                 <Link to="/sign_up" className="about-catch">
                     <h1>{strings.catch}</h1>
                     <button type="button">{strings.catchCTA}</button>
                 </Link>
-                <div className="about-bullets">
-                    <p>{strings.catchHighlight}</p>
-                    <ul>
-                        {strings.bullets.map((bullet) => (
-                            <li>{bullet}</li>
-                        ))}
-                    </ul>
-                </div>
+            </div>
             </div>
 
-            <div className="about-more">
-                {strings.more.map((item) => (
-                    <div>
-                        <h2>{item.title}</h2>
-                        <p>{item.desc}</p>
-                    </div>
-                ))}
-            </div>
+
         </div>
     );
 }
