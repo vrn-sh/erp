b17dd4240412bc9fc2498a1e83a7d211c0e49511
Loris Reggiani <loris.reggiani@epitech.eu>
2023-10-11 21:54:13 +0200

    [FIX] typing for PR

--- web/src/pages/Home/Plan/Plan.tsx
+++ web/src/pages/Home/Plan/Plan.tsx
@@ -19,7 +19,6 @@ function CustomLink({
     );
 }
 
-
 const subscriptions = [
     {
         type: 'Abonnement',
@@ -44,9 +43,9 @@ const subscriptions = [
 const cardColors = ['#CAB9FF', '#B299FF', '#A385FF'];
 
 export default function Plan() {
-    const [stoppedCards, setStoppedCards] = useState([]);
+    const [stoppedCards, setStoppedCards] = useState<number[]>([]);
 
-    const cardRefs = useRef([]);
+    const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
 
     useEffect(() => {
         const observerOptions = {
@@ -54,57 +53,58 @@ export default function Plan() {
             rootMargin: '0px',
             threshold: 0.1,
         };
-    const handleIntersect = (
-        entries: IntersectionObserverEntry[], 
-        observer: IntersectionObserver
-    ) => {
-        entries.forEach((entry: IntersectionObserverEntry) => {
-            if (entry.isIntersecting && entry.target instanceof HTMLElement) {
-                const dataIndex = entry.target.dataset.index;
-                if (dataIndex && typeof dataIndex === 'string') {
-                    const index = Number(dataIndex);
-                    if (!isNaN(index) && !stoppedCards.includes(index)) {
-                        entry.target.classList.add('visible');
-                        observer.unobserve(entry.target);
+
+        const handleIntersect = (
+            entries: IntersectionObserverEntry[],
+            observer: IntersectionObserver
+        ) => {
+            entries.forEach((entry: IntersectionObserverEntry) => {
+                if (entry.isIntersecting && entry.target instanceof HTMLElement) {
+                    const dataIndex = entry.target.dataset.index;
+                    if (dataIndex && typeof dataIndex === 'string') {
+                        const index = Number(dataIndex);
+                        if (!isNaN(index) && !stoppedCards.includes(index)) {
+                            entry.target.classList.add('visible');
+                            observer.unobserve(entry.target);
+                        }
                     }
                 }
+            });
+        };
+
+        const observer: IntersectionObserver = new IntersectionObserver((entries) => handleIntersect(entries, observer), observerOptions);
+
+        cardRefs.current.forEach((card, index) => {
+            if (card) {
+                observer.observe(card);
             }
         });
-    };
 
-
-    const observer: IntersectionObserver = new IntersectionObserver((entries) => handleIntersect(entries, observer), observerOptions);
-
-    cardRefs.current.forEach((card, index) => {
-        observer.observe(card);
-    });
-
-    return () => {
-        observer.disconnect();
-    };
+        return () => {
+            observer.disconnect();
+        };
     }, [stoppedCards]);
-
 
     return (
         <div id="plan" className="plan">
             <h1 className='title'>Ready to start your journey ?</h1>
             <div className="subscription-container">
-            {subscriptions.map((subscription, index) => (
-                <div
-                    className={`subscription-card ${stoppedCards.includes(index) ? 'stopped' : ''}`}
-                    key={index}
-                    ref={(ref) => (cardRefs.current[index] = ref)}
-                    style={{ backgroundColor: cardColors[index] }}
-                >
-                <h2>{subscription.type}</h2>
-                <p>{subscription.price}</p>
-                <p>{subscription.recurrence}</p>
-                <div className='button-with'>    
-                    <button className="souscrire-button">S'ouscrire</button>
-                </div>
-                <p>{subscription.hypothesis}</p>
-                </div>
-            ))}
+                {subscriptions.map((subscription, index) => (
+                    <div
+                        className={`subscription-card ${stoppedCards.includes(index) ? 'stopped' : ''}`}
+                        key={index}
+                        ref={(ref) => (cardRefs.current[index] = ref)}
+                        style={{ backgroundColor: cardColors[index] }}
+                    >
+                        <h2>{subscription.type}</h2>
+                        <p>{subscription.price}</p>
+                        <p>{subscription.recurrence}</p>
+                        <div className='button-with'>
+                            <button className="souscrire-button">S'ouscrire</button>
+                        </div>
+                        <p>{subscription.hypothesis}</p>
+                    </div>
+                ))}
             </div>
             <h3 className='pro'>Besoin d'un contrat pro ?</h3>
             <div className='button-with'>
