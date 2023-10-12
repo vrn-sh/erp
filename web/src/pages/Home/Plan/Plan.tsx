e6d80c8308cefdb162b1a0a35cd93ac5e7443267
clara mac <claraxu1201@gmail.com>
2023-10-12 22:16:28 +0200

    (web: part plan done

--- web/src/pages/Home/Plan/Plan.tsx
+++ web/src/pages/Home/Plan/Plan.tsx
@@ -1,31 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './Plan.scss'; // Import your SCSS file
+import * as AiIcons from 'react-icons/ai';
 import subscriptions from '../../../assets/strings/en/subscriptions.json';
-
-function CustomLink({
-    to,
-    children,
-    className,
-    ...props
-}: {
-    to: string;
-    children: string;
-    className: string;
-}) {
-    const buttonStyle = {
-        fontFamily: 'Poppins',
-    };
-
-    return (
-        <button className={className} onClick={() => window.location.href = to} style={buttonStyle} {...props}>
-            {children}
-        </button>
-    );
-}
 
 export default function Plan() {
     const [stoppedCards, setStoppedCards] = useState<number[]>([]);
-    const cardColors = ['primary-color', 'secondary-color', 'tertiary-color']; // Define your color classes
+    const cardColors = ['primary-color', 'secondary-color', 'primary-color']; // Define your color classes
 
     const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
 
@@ -36,13 +16,19 @@ export default function Plan() {
             threshold: 0.1,
         };
 
-        const handleIntersect = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+        const handleIntersect = (
+            entries: IntersectionObserverEntry[],
+            observer: IntersectionObserver
+        ) => {
             entries.forEach((entry: IntersectionObserverEntry) => {
                 // Intersection Observer logic remains the same
             });
         };
 
-        const observer: IntersectionObserver = new IntersectionObserver((entries) => handleIntersect(entries, observer), observerOptions);
+        const observer: IntersectionObserver = new IntersectionObserver(
+            (entries) => handleIntersect(entries, observer),
+            observerOptions
+        );
 
         cardRefs.current.forEach((card, index) => {
             if (card) {
@@ -57,27 +43,49 @@ export default function Plan() {
 
     return (
         <div id="plan" className="plan-title">
-            <h1 className='plan-title'>Ready to start your journey ?</h1>
+            <h1 className="plan-title">Ready to start your journey ?</h1>
             <div className="plan-subscription-container">
                 {subscriptions.map((subscription, index) => (
                     <div
-                        className={`subscription-card ${stoppedCards.includes(index) ? 'stopped' : ''} ${cardColors[index % cardColors.length]}`}
-                        key={index}
-                        ref={(ref) => (cardRefs.current[index] = ref)}
+                        className={`subscription-card ${
+                            cardColors[index % cardColors.length]
+                        }`}
+                        ref={(ref) => {
+                            cardRefs.current[index] = ref;
+                        }}
                     >
                         <h2>{subscription.type}</h2>
-                        <p>{subscription.price}</p>
-                        <p>{subscription.recurrence}</p>
-                        <div className='button-with'>
-                            <button className="souscrire-button">Souscrire</button>
-                        </div>
-                        <p>{subscription.hypothesis}</p>
+                        <label>€ {subscription.price}</label>
+                        <p className="plan-small-des">
+                            {subscription.recurrence}
+                        </p>
+                        {subscription.service.map((s) => {
+                            return (
+                                <div className="plan-service-container">
+                                    <AiIcons.AiOutlineCheck
+                                        style={{ color: 'green' }}
+                                    />
+                                    <p>{s}</p>
+                                </div>
+                            );
+                        })}
+                        <button className="souscrire-button" type="button">
+                            Souscrire
+                        </button>
                     </div>
                 ))}
             </div>
-            <h3 className='pro'>Besoin d'un contrat pro ?</h3>
-            <div className='button-with'>
-                <CustomLink to="#contact" className="contact-button">Contacter Voron</CustomLink>
+            <h5 className="plan-pro">Need a plan more personalised?</h5>
+            <div className="button-with">
+                <button
+                    type="button"
+                    className="plan-contact-button"
+                    onClick={() => {
+                        window.location.href = '#contact';
+                    }}
+                >
+                    Contact us
+                </button>
             </div>
         </div>
     );
