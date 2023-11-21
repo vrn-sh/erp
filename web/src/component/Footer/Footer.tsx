8c7fb07b6077a47fee001cc23c5ac010b58dae88
Loris Reggiani <loris.reggiani@epitech.eu>
2023-11-21 03:49:31 +0100

    feat(web): adding newsletter inside the footer

--- web/src/component/Footer/Footer.tsx
+++ web/src/component/Footer/Footer.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Footer.scss';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/voron-logo.svg';
+import axios from 'axios';
+import config from '../../config';
+import '../../pages/Login/Login.scss';
 
 function CustomLink({
     to,
@@ -21,6 +24,27 @@ function CustomLink({
 }
 
 export default function Footer() {
+    const [mailInput, setMailInput] = useState("");
+    const isValidEmail = (email: string) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    };
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        if (!isValidEmail(mailInput)) {
+            alert('Please enter a valid email address');
+            return;
+        }
+        try {
+            const response = await axios.post(
+                `${config.apiUrl}/mailing-list`,
+                { email: mailInput }
+            );
+            console.log('Mailing List Response:', response);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
     return (
         <div className="footer">
             <img
@@ -32,6 +56,19 @@ export default function Footer() {
                 voron
             </Link>
             <p>Copyright ©2023 VORON, Inc.</p>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <input
+                    style={{ margin: '10px' }}
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={(e) => setMailInput(e.target.value)}
+                    placeholder="Enter your email"
+                />
+                <button onClick={handleSubmit} type="button" style={{ width: '3em' }}>
+                    Subscribe to our newsletter
+            </button>
+            </div>
             <p>Contact voron@djnn.sh</p>
             <div className="policy-link">
                 <ul>
