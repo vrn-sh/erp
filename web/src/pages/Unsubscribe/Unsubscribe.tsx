ee374388ff32095ca2ae3fd6563f32ce8243ca6a
Loris Reggiani <loris.reggiani@epitech.eu>
2023-12-07 08:43:25 +0100

    feat(web): changing the loading of the unsubscribe page

--- web/src/pages/Unsubscribe/Unsubscribe.tsx
+++ web/src/pages/Unsubscribe/Unsubscribe.tsx
@@ -1,55 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
-import * as MdIcons from 'react-icons/md';
-import config from '../../config';
 import './Unsubscribe.scss';
 import logo from '../../assets/voron-logo.svg';
 
 export default function UnsubscribeEmail() {
-    const [query, setQuery] = useSearchParams();
-    const token = query.get('token');
-    const [confirmed, setConfirmed] = useState(false);
-    const user = localStorage.getItem('user_info');
-    const pwd = user !== null ? JSON.parse(user).password : '';
+    const [query] = useSearchParams();
+    const email = query.get('email');
+    const [unsubscribed, setUnsubscribed] = useState(false);
+    const [error, setError] = useState('');
 
-    const confirmFun = async () => {
-        await axios
-            .post(`${config.apiUrl}/confirm?token=${token}`, {
-                password: pwd,
-            })
-            .then(() => {
-                setConfirmed(true);
-            })
-            .catch((error) => {
-                throw error;
+    const unsubscribeEmail = async () => {
+        try {
+            await axios.post(`http://localhost:8000/mailing-list/unsubscribe`, {
+                email,
             });
+            setUnsubscribed(true);
+        } catch (err) {
+            setError('Error unsubscribing. Please try again.');
+        }
     };
+
+    useEffect(() => {
+        if (email) {
+            unsubscribeEmail();
+        }
+    }, [email]);
 
     return (
         <div className="container unsubscribe">
             <div className="row">
                 <div className="col-md-12">
                     <img src={logo} alt="" className="unsubscribe-logo" />
-                    <h4>
-                        You've successfully been unsubscribed from Voron's
-                        newsletter.
-                    </h4>
-                    {/* <p>Didn't mean to unsubscribe?</p>
-                        <form
-                            action="/lists/encryptedResubscribe?token=..."
-                            className="form-horizontal"
-                            method="POST"
-                        >
-                            <input
-                                type="hidden"
-                                name="signature"
-                                value="..."
-                            />
-                            <button className="btn btn-success btn-lg" type="submit">
-                                <i className="fa fa-plus"></i> Resubscribe
-                            </button>
-                        </form> */}
+                    {unsubscribed ? (
+                        <h4>
+                            You've successfully been unsubscribed from Voron's
+                            newsletter.
+                        </h4>
+                    ) : (
+                        <h4>{error || 'Unsubscribing...'}</h4>
+                    )}
                 </div>
             </div>
         </div>
