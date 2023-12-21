8f80ea0fb1ce5af8360606e26dac879aa30339bd
Loris Reggiani <loris.reggiani@epitech.eu>
2023-12-21 01:10:07 +0100

    feat(api): removing the duplicated files + updating migration file

--- api/api/serializers/mailingList.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from api.models.mailingList import MailingListItem
-
-
-class MailingListItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MailingListItem
-        fields = ['email']
