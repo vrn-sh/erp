88b65baac767c4690e654693b8c3bce84e0d0232
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-12-26 18:38:57 +0100

    feat(api): Finalize MFA (#386)
    * feat(api): enable mfa and added has_otp to auth model
    
    * fix(ci): typo in `requirements.txt`
    
    ---------
    
    Co-authored-by: winds0r <wontsleep@protonmail.com>

--- api/core/urls.py
+++ api/core/urls.py
@@ -12,12 +12,13 @@ from drf_yasg import openapi
 from knox.views import LogoutView
 
 from api.views import LoginView, PingView, ConfirmAccountView, ResetPasswordView
-from api.views.report.report import GeneratePDFReportView, GenerateMDReportView
 from api.views.hunter import HunterView, SaasProxyView, WappProxyView
+from api.views.search import SearchView
+from api.views.mfa import MFAView
+from api.views.report.report import GeneratePDFReportView, GenerateMDReportView
 from api.views.viewsets import FreelancerViewset, RegisterViewset, PentesterViewset, ManagerViewset, TeamViewset
 from api.views.viewsets.vulns import NotesViewset, VulnerabilityViewset, VulnTypeViewset
 from api.views.viewsets.mission import CredentialViewset, MissionViewset, NmapViewset, CrtShView, WappalyzerRequestView
-from api.views.search import SearchView
 from api.views.viewsets.client_info import ClientInfoViewset
 from api.views.viewsets.mailing_list import MailingListViewset
 
@@ -51,20 +52,19 @@ router.register(r'mailing-list', MailingListViewset)
 
 
 urlpatterns = [
-    path(r'crtsh', CrtShView.as_view()),
-    path(r'login', LoginView.as_view(), name='knox_login'),
-    path('logout', LogoutView.as_view(), name='knox_logout'),
-    path('ping', PingView.as_view()),
-    path('confirm', ConfirmAccountView.as_view()),
-    path('reset', ResetPasswordView.as_view(), name='reset_password'),
-    path('register', RegisterViewset.as_view({'post': 'create'})),
-    re_path(r'^docs/$', SchemaView.with_ui('redoc',
-            cache_timeout=0), name='schema-redoc'),
-    path(r'download-report',
-         GeneratePDFReportView.as_view({'post': 'create'})),
-    path(r'markdown-report', GenerateMDReportView.as_view()),
-    path('search', SearchView.as_view()),
-    path(r'hunt', HunterView.as_view()),
-    path(r'wapp', WappProxyView.as_view()),
-    path(r'saas', SaasProxyView.as_view()),
+   path(r'crtsh', CrtShView.as_view()),
+   path(r'login', LoginView.as_view(), name='knox_login'),
+   path('mfa', MFAView.as_view()),
+   path('logout', LogoutView.as_view(), name='knox_logout'),
+   path('ping', PingView.as_view()),
+   path('confirm', ConfirmAccountView.as_view()),
+   path('reset', ResetPasswordView.as_view()),
+   path('register', RegisterViewset.as_view({'post': 'create'})),
+   re_path(r'^docs/$', SchemaView.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path(r'download-report', GeneratePDFReportView.as_view({'post': 'create'})),
+   path(r'markdown-report', GenerateMDReportView.as_view()),
+   path('search', SearchView.as_view()),
+   path(r'hunt', HunterView.as_view()),
+   path(r'wapp', WappProxyView.as_view()),
+   path(r'saas', SaasProxyView.as_view())
 ] + router.urls
