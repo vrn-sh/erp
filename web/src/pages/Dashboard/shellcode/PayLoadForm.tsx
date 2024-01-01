0578ffe1c77b5f6eb8a904f478bc5f3599ae64c1
Loris-Reggiani <loris.reggiani@epitech.eu>
2024-01-01 11:55:02 +0100

    feat(web): Adding Documentation.tsx + new GIF to explain how it works

--- web/src/pages/Dashboard/shellcode/PayLoadForm.tsx
+++ web/src/pages/Dashboard/shellcode/PayLoadForm.tsx
@@ -1,6 +1,7 @@
 import Modal from 'react-modal';
 import React, { useState } from 'react';
 import FormControl from '@mui/material/FormControl';
+
 import {
     InputLabel,
     MenuItem,
@@ -11,6 +12,7 @@ import {
 import BasicShellcode from './BasicShellcode';
 import MyPhShellcode from './MyPhShellcode';
 import LinkDisplay from './LinkDisplay';
+import { MyPhDocumentationContent, BasicShellcodeDocumentationContent } from './Documentation';
 
 export default function PayLoadForm(props: {
     isModalOpen: boolean;
@@ -20,7 +22,15 @@ export default function PayLoadForm(props: {
     const [payloadType, setPayloadType] = useState('myph');
     const [link, setLink] = useState('');
     const [error, setError] = useState('');
+    const [isNewModalOpen, setIsNewModalOpen] = useState(false);
 
+    const openNewModal = () => {
+        setIsNewModalOpen(true);
+    };
+
+    const closeNewModal = () => {
+        setIsNewModalOpen(false);
+    };
     const handleChange = (event: SelectChangeEvent) => {
         setError('');
         setPayloadType(event.target.value as string);
@@ -45,29 +55,56 @@ export default function PayLoadForm(props: {
                 isOpen={isModalOpen}
                 onRequestClose={() => close()}
                 contentLabel="General Payload Modal"
-                style={{
-                    content: {
-                        border: '1px solid #ccc',
-                        borderRadius: '10px',
-                        maxWidth: '300em',
-                        left: '50%',
-                        transform: 'translate(-50%, 0)',
-                        overflow: 'hidden',
-                        inset: '40px 40px auto 50%',
-                    },
-                }}
+                                style={{
+                                    content: {
+                                        border: '1px solid #ccc',
+                                        borderRadius: '10px',
+                                    },
+                                }}
             >
                 {link !== '' ? (
                     <LinkDisplay link={link} close={() => close()} />
                 ) : (
                     <>
-                        <h2
+                        <div
                             style={{
-                                fontFamily: 'Poppins-Regular',
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
                             }}
                         >
-                            Payload generation
-                        </h2>
+                            <h2>General payload</h2>
+                            <button
+                                type="button"
+                                className="btn"
+                                onClick={openNewModal}
+                            >
+                                Documentation {payloadType === 'myph' ? 'MyPH' : 'Basic Shellcode'}
+                                </button>
+                                <Modal
+                                    isOpen={isNewModalOpen}
+                                    onRequestClose={closeNewModal}
+                                    contentLabel="Documentation Modal"
+                                    style={{
+                                        content: {
+                                            border: '1px solid #ccc',
+                                            borderRadius: '10px',
+                                            position: 'absolute',
+                                            overflow: 'scroll',
+                                            fontFamily: 'Poppins-Medium',
+                                        },
+                                    }}
+                                >
+                                    {payloadType === 'myph' && <MyPhDocumentationContent />}
+                                    {payloadType === 'basicshellcode' && <BasicShellcodeDocumentationContent />}
+                                    <button
+                                        type="button"
+                                        onClick={closeNewModal}
+                                    >
+                                        Close
+                                    </button>
+                                </Modal>
+                        </div>
                         <InputLabel
                             style={{
                                 fontFamily: 'Poppins-Regular',
