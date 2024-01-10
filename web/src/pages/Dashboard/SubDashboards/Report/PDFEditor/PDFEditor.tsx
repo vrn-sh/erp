8585b9df8f378c4bd77e0f0cb784567a697e5072
djnn.sh <email@djnn.sh>
2024-01-10 23:43:19 +0100

    hacky attempt

--- web/src/pages/Dashboard/SubDashboards/Report/PDFEditor/PDFEditor.tsx
+++ web/src/pages/Dashboard/SubDashboards/Report/PDFEditor/PDFEditor.tsx
@@ -20,7 +20,7 @@ export default function PdfViewerComponent(props: PdfViewerProps): ReactElement 
           container: containerRef.current!,
           // The document to open.
           document: props.document,
-          baseUrl: `${window.location.protocol}//${window.location.host}/public/`,
+          baseUrl: `${window.location.protocol}//${window.location.host}/assets/`,
           toolbarItems: [...PSPDFKit.defaultToolbarItems, {type: "content-editor"}],
         }).then((instance) => {
           // PSPDFKit is ready to be used.
