158ec2d173ddf18444634bbb3bc36c65fa2427a8
djnn.sh <email@djnn.sh>
2024-01-12 06:13:45 +0100

    fix(web): linter

--- web/src/pages/Dashboard/SubDashboards/Report/PDFEditor/PDFEditor.tsx
+++ web/src/pages/Dashboard/SubDashboards/Report/PDFEditor/PDFEditor.tsx
@@ -1,43 +1,54 @@
-import { useEffect, useRef, ReactElement } from "react";
-import PSPDFKit from "pspdfkit";
+import { useEffect, useRef, ReactElement } from 'react';
+import PSPDFKit from 'pspdfkit';
 
 interface PdfViewerProps {
-  document: string; // Assuming `document` is a string representing the document URL
+    document: string; // Assuming `document` is a string representing the document URL
 }
 
-export default function PdfViewerComponent(props: PdfViewerProps): ReactElement {
-  const containerRef = useRef<HTMLDivElement>(null);
+export default function PdfViewerComponent(
+    props: PdfViewerProps
+): ReactElement {
+    const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const container = containerRef.current;
-      const loadPSPDFKit = async () => {
-      try {
-        // Dynamically import pspdfkit during runtime
+    useEffect(() => {
+        const container = containerRef.current;
+        const loadPSPDFKit = async () => {
+            try {
+                // Dynamically import pspdfkit during runtime
 
-        // Now you can use pspdfkit
-        // For example:
-        PSPDFKit.unload(container);
+                // Now you can use pspdfkit
+                // For example:
+                PSPDFKit.unload(container);
 
-        PSPDFKit.load({
-          licenseKey: import.meta.env.VITE_REACT_APP_PSPDFKIT_LICENSE_KEY,
-          // Container where PSPDFKit should be mounted.
-          container: containerRef.current!,
-          // The document to open.
-          document: props.document,
-          baseUrl: `${window.location.protocol}//${window.location.host}/assets/`,
-          toolbarItems: [...PSPDFKit.defaultToolbarItems, {type: "content-editor"}],
-        }).then(() => {
-          console.log("PSPDFKit for Web successfully loaded!!!");
-        });
-      } catch (error) {
-        console.error('Error loading PSPDFKit:', error);
-      }
-    };
+                PSPDFKit.load({
+                    licenseKey: import.meta.env
+                        .VITE_REACT_APP_PSPDFKIT_LICENSE_KEY,
+                    // Container where PSPDFKit should be mounted.
+                    container: containerRef.current!,
+                    // The document to open.
+                    document: props.document,
+                    baseUrl: `${window.location.protocol}//${window.location.host}/assets/`,
+                    toolbarItems: [
+                        ...PSPDFKit.defaultToolbarItems,
+                        { type: 'content-editor' },
+                    ],
+                }).then(() => {
+                    console.log('PSPDFKit for Web successfully loaded!!!');
+                });
+            } catch (error) {
+                console.error('Error loading PSPDFKit:', error);
+            }
+        };
 
-    loadPSPDFKit();
+        loadPSPDFKit();
+    }, [props.document]);
 
-  }, [props.document]);
-
-  // This div element will render the document to the DOM.
-  return <div id="pdf-editor" ref={containerRef} style={{ width: "100%", height: "100vh" }} />;
+    // This div element will render the document to the DOM.
+    return (
+        <div
+            id="pdf-editor"
+            ref={containerRef}
+            style={{ width: '100%', height: '100vh' }}
+        />
+    );
 }
