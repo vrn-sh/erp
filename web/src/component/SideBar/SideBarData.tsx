f60366fc8c57ffe821010e64e8989840fc2aa859
Ananane <anne.mboke@epitech.eu>
2024-01-15 21:23:24 +0100

    feat(web): client list (#401)
    Co-authored-by: yuhuixx <yuhui@flitter.fr>

--- web/src/component/SideBar/SideBarData.tsx
+++ web/src/component/SideBar/SideBarData.tsx
@@ -132,6 +132,15 @@ const SideBarData = function SideBarDataF() {
             isForManager: false,
             subNav: [],
         },
+        {
+            title: 'Clients',
+            path: '/client', // change when build client page
+            icon: <IoIcons.IoMdPeople color="#7c44f3" />,
+            iconClosed: <IoIcons.IoIosArrowForward />,
+            iconOpened: <IoIcons.IoIosArrowDown />,
+            isForManager: true,
+            subNav: [],
+        },
     ];
 };
 
