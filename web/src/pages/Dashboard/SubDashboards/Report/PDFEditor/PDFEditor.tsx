a25d822fe98ac2c71f80ba18d3b212339e412ba1
lambdina <pirouettecacahuete@protonmail.com>
2024-01-17 17:25:11 +0100

    xip

--- web/src/pages/Dashboard/SubDashboards/Report/PDFEditor/PDFEditor.tsx
+++ web/src/pages/Dashboard/SubDashboards/Report/PDFEditor/PDFEditor.tsx
@@ -9,49 +9,58 @@ interface PdfViewerProps {
 }
 
 export default function PdfViewerComponent(
-    props: PdfViewerProps
+  props: PdfViewerProps
 ): ReactElement {
-    const containerRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const container = containerRef.current;
     const loadPSPDFKit = async () => {
-        PSPDFKit.unload(container);
-        const instance = await PSPDFKit.load({
-          autoSaveMode: PSPDFKit.AutoSaveMode.DISABLED,
-          licenseKey: import.meta.env.VITE_REACT_APP_PSPDFKIT_LICENSE_KEY,
-          container: containerRef.current!,
-          document: props.document,
-          baseUrl: `${window.location.protocol}//${window.location.host}/assets/`,
-          toolbarItems: [
-            ...PSPDFKit.defaultToolbarItems.filter((item) => item.type !== "export-pdf"),
-            { type: "content-editor" },
-          ],
-        });
-        if (instance) {
-          instance.setToolbarItems((items) => [
-            ...items,
-            {
-              type: "custom",
-              title: "Save",
-              onPress: async () => {
-                console.log("Pressed this mf button");
-              },
+      PSPDFKit.unload(container);
+      const instance = await PSPDFKit.load({
+        autoSaveMode: PSPDFKit.AutoSaveMode.DISABLED,
+        licenseKey: import.meta.env.VITE_REACT_APP_PSPDFKIT_LICENSE_KEY,
+        container: containerRef.current!,
+        document: props.document,
+        baseUrl: `${window.location.protocol}//${window.location.host}/assets/`,
+        toolbarItems: [
+          ...PSPDFKit.defaultToolbarItems.filter((item) => item.type !== "export-pdf"),
+          { type: "content-editor" },
+        ],
+      });
+      if (instance) {
+        instance.setToolbarItems((items) => [
+          ...items,
+          {
+            type: "custom",
+            title: "Save",
+            onPress: async () => {
+              console.log("Pressed this mf button");
+              const arrayBuffer = await (instance as Instance).exportPDF();
+              const blob = new Blob([arrayBuffer], { type: 'application/pdf' });
+              const formData = new FormData();
+              formData.append("mission", props.mission?.toString() || "");
+              formData.append("template_name", props.template || "");
+              formData.append("file", blob);
+              await fetch("/download-report", {
+                method: "POST",
+                body: formData,
+              });
             },
-          ]);
-          console.log(instance.toolbarItems)
-        }
+          },
+        ]);
+      }
     };
 
-        loadPSPDFKit();
-    }, [props.document]);
+    loadPSPDFKit();
+  }, [props.document]);
 
-    // This div element will render the document to the DOM.
-    return (
-        <div
-            id="pdf-editor"
-            ref={containerRef}
-            style={{ width: '100%', height: '100vh' }}
-        />
-    );
+  // This div element will render the document to the DOM.
+  return (
+    <div
+      id="pdf-editor"
+      ref={containerRef}
+      style={{ width: '100%', height: '100vh' }}
+    />
+  );
 }
